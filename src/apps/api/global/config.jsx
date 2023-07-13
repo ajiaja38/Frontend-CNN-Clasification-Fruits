ff69f4e939ad671d6845fd7a218913703fb254da
AjiPerdana38 <ajicooljazz38@gmail.com>
2023-07-13 20:12:49 +0700

    update endpoint flask

--- src/apps/api/global/config.jsx
+++ src/apps/api/global/config.jsx
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'http://54.79.171.122:5000',
-  BASE_URL_PREDICTION: 'http://localhost:5000'
+  BASE_URL_PREDICTION: 'http://localhost:5050'
 }
 
 export default CONFIG
